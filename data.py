7ace835848ef77d8d8c60f57934a83e3a2f13b6b
Haesung Jeon <seastar105@gmail.com>
2024-12-25 16:23:57 +0900

    pre-commit and use sdpa

--- data.py
+++ data.py
@@ -1,38 +1,33 @@
-from torch.utils.data import Dataset
-from transformers import WhisperProcessor
-from transformers import WhisperTokenizer, WhisperFeatureExtractor
-from datasets import load_dataset
-import librosa
 from dataclasses import dataclass
 from typing import Any, Dict, List, Union
+
+import librosa
 import torch
+from transformers import WhisperFeatureExtractor, WhisperTokenizer
 
 
 class WhisperAudioCaptionProcessor:
     def __init__(self, model_name: str):
         # use translate task as captioning task now
-        self.tokenizer = WhisperTokenizer.from_pretrained(model_name, task="translate")
+        self.tokenizer = WhisperTokenizer.from_pretrained(model_name, language="en", task="translate")
         self.feature_extractor = WhisperFeatureExtractor.from_pretrained(model_name)
-    
+
     def __call__(self, item):
         caption = item["json"]["text"]
         audio = item["flac"]["array"]
         sr = item["flac"]["sampling_rate"]
         if sr != 16000:
             audio = librosa.resample(audio, orig_sr=sr, target_sr=16000)
-        
+
         input_features = self.feature_extractor(audio, sampling_rate=16000, return_tensors="pt").input_features[0]
         input_ids = self.tokenizer(caption).input_ids
-        return {
-            "input_features": input_features,
-            "labels": input_ids
-        }
+        return {"input_features": input_features, "labels": input_ids}
+
 
 # from https://huggingface.co/blog/fine-tune-whisper
 @dataclass
 class DataCollatorSpeechSeq2SeqWithPadding:
     processor: Any
-    decoder_start_token_id: int
 
     def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
         # split inputs and labels since they have to be of different lengths and need different padding methods
@@ -47,12 +42,6 @@ class DataCollatorSpeechSeq2SeqWithPadding:
 
         # replace padding with -100 to ignore loss correctly
         labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
-
-        # if bos token is appended in previous tokenization step,
-        # cut bos token here as it's append later anyways
-        if (labels[:, 0] == self.decoder_start_token_id).all().cpu().item():
-            labels = labels[:, 1:]
-
         batch["labels"] = labels
-        
+
         return batch
--- train.py
+++ train.py
@@ -1,40 +1,44 @@
-from datasets import load_dataset
-from data import WhisperAudioCaptionProcessor, DataCollatorSpeechSeq2SeqWithPadding
-from transformers import WhisperForConditionalGeneration, WhisperConfig
 import hydra
+from datasets import load_dataset
 from omegaconf import DictConfig, OmegaConf
-from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
+from transformers import (
+    Trainer,
+    TrainingArguments,
+    WhisperConfig,
+    WhisperForConditionalGeneration,
+)
+
+from data import DataCollatorSpeechSeq2SeqWithPadding, WhisperAudioCaptionProcessor
 
 
 @hydra.main(version_base=None, config_path="configs", config_name="main")
 def main(config: DictConfig):
     model_config = config.model_config
     data_config = config.data_config
-    
+
     # Load model
     whisper_config = WhisperConfig.from_pretrained(model_config.name)
     whisper_config._attn_implementation = model_config.attn_implementation
     whisper_model = WhisperForConditionalGeneration.from_pretrained(model_config.name, config=whisper_config)
-    
+
     # use translate task as captioning task
     whisper_model.generation_config.task = "translate"
+    whisper_model.generation_config.language = "en"
     whisper_model.generation_config.forced_decoder_ids = None
-    
+
     # Load the dataset
     processor = WhisperAudioCaptionProcessor(model_config.name)
-    collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor, decoder_start_token_id=whisper_model.config.decoder_start_token_id)
-    
+    collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
+
     dataset = load_dataset(data_config.name, split="train", streaming=True).map(processor, batched=False)
-    
+
     # Load trainer
     trainer_config = OmegaConf.to_container(config.trainer_config, resolve=True)
-    trainer = Seq2SeqTrainer(
-        model=whisper_model,
-        args=Seq2SeqTrainingArguments(**trainer_config),
-        data_collator=collator,
-        train_dataset=dataset
+    trainer = Trainer(
+        model=whisper_model, args=TrainingArguments(**trainer_config), data_collator=collator, train_dataset=dataset
     )
     trainer.train()
+
 
 if __name__ == "__main__":
     main()
